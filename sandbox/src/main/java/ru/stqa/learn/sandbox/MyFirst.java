05f2a367fbab15f3dff38546168f47a4c186ed7b
Obaifa <lines06@yandex.ru>
2022-04-23 14:32:22 +0700

    Добавлено вычисление площади квадрата

--- sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirst.java
@@ -1,5 +1,10 @@
 package ru.stqa.learn.sandbox;
 
 public class MyFirst {
-    public static void main(String[] args) {System.out.print("Hello, world!"); }
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int l=5;
+        int s=l*l;
+        System.out.println("Площадь квадрата со стороной " +l+ "=" +s);
+    }
 }
